ef00aa5c386702cdecd00f2ebaadace4e73e40bf
tom60914 <tom60914@gmail.com>
2014-11-18 18:57:06 +0800

    Add Main screen and some animation in screen

--- gen/com/example/showcho_man/R.java
+++ gen/com/example/showcho_man/R.java
@@ -8,6 +8,9 @@
 package com.example.showcho_man;
 
 public final class R {
+    public static final class anim {
+        public static final int rotate=0x7f040000;
+    }
     public static final class attr {
     }
     public static final class dimen {
@@ -18,25 +21,32 @@ public final class R {
          would include 7" and 10" devices in landscape (~960dp and ~1280dp respectively).
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int all_logo=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int icon5=0x7f020002;
+        public static final int showcho_logo2=0x7f020003;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int LinearLayout1=0x7f090000;
+        public static final int Relative_out=0x7f090001;
+        public static final int action_settings=0x7f090004;
+        public static final int icon=0x7f090002;
+        public static final int marquee=0x7f090003;
     }
     public static final class layout {
         public static final int main=0x7f030000;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050002;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050001;
+        public static final int action_settings=0x7f060002;
+        public static final int app_name=0x7f060000;
+        public static final int hello_world=0x7f060001;
     }
     public static final class style {
         /** 
@@ -60,10 +70,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
--- src/com/example/showcho_man/MainActivity.java
+++ src/com/example/showcho_man/MainActivity.java
@@ -1,17 +1,60 @@
 package com.example.showcho_man;
 
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.view.View.OnClickListener;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.TranslateAnimation;
+import android.widget.ImageView;
 
 
 public class MainActivity extends Activity {
-
+	
+	ImageView iconbutton;
+	ImageView image_marquee;
+	
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    	
+    	this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+    	super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        DisplayMetrics dm = new DisplayMetrics();   
+        getWindowManager().getDefaultDisplay().getMetrics(dm); //先取得螢幕解析度  
+        int width = (dm.widthPixels);
+        int height = (dm.heightPixels);
+        
+        // All object declared
+        iconbutton = (ImageView)findViewById(R.id.icon);
+        image_marquee = (ImageView)findViewById(R.id.marquee);
+        final Animation animRotateIn_icon = AnimationUtils.loadAnimation(this, R.anim.rotate);
+        
+        iconbutton.setOnClickListener(new OnClickListener(){
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+					iconbutton.startAnimation(animRotateIn_icon);
+			}			
+        });
+        
+        TranslateAnimation am = new TranslateAnimation(-width,width, 0, 0);
+        //設定移動路徑(x1,x2,y1,y2);
+        am.setDuration(7000);
+        // 動畫開始到結束的時間(毫秒)
+        am.setRepeatCount(-1);
+        //0代表只跑一次,-1代表一直重複
+        am.startNow();
+        //動畫開始
+        image_marquee.setAnimation(am);
     }
 
 
